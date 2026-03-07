2a1acd2520a5f51c114b7a362aca6d07707ec26e
QaisarGit <qaisar.fareed@gmail.com>
2026-03-07 16:29:13 +0530

    git cic d 1000

--- src/test/java/Tests/googleTest.java
+++ src/test/java/Tests/googleTest.java
@@ -5,20 +5,25 @@ import org.testng.annotations.Test;
 
 import Base.baseClass;
 
-public class googleTest extends baseClass{
-	
-	 @Test
-	    public void testGoogleTitle() {
-	        setup();
-	        driver.get("https://www.google.com");
-	        System.out.println(driver.getTitle());
-	        tearDown();
-	        System.out.println("success ====================== ");
-	        
-	        System.out.println("success ====================== ");
+public class googleTest extends baseClass {
 
-	        System.out.println("success ====================== ");
+	@Test
+	public void testGoogleTitle() {
+		setup();
+		driver.get("https://www.google.com");
+		System.out.println("title of the page is   :  "+driver.getTitle());
+		
+		tearDown();
+		System.out.println("success ====================== ");
 
-	 }
+		System.out.println("success ====================== ");
+		
 
+	}
+
+	@Test
+	public void  test() {
+		
+		System.out.println("testing ++++++++++++++++++++++++");
+	}
 }
